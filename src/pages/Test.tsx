df67768466c9b33b47c9a768b6cc18fc98a5402a
Boluwatife Olaniyan <42306524+devkijified@users.noreply.github.com>
2026-04-29 00:23:48 +0100

    Create Test.tsx

---
+++ src/pages/Test.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Test Page - If you see this, React is working!</h1>
+      <p>The app is loading correctly.</p>
+    </div>
+  );
+}
